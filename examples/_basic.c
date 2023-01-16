b1e7ac10356a26536d47d2fa6767ea783726551b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-01-16 09:47:09 -0800

    comment our C example

--- examples/_basic.c
+++ examples/_basic.c
@@ -12,20 +12,33 @@ xev_cb_action timerCallback(
 }
 
 int main(void) {
+    // Initialize the loop state. Notice we can use a stack-allocated
+    // value here. We can even pass around the loop by value! The loop
+    // will contain all of our "completions" (watches).
     xev_loop loop;
     if (xev_loop_init(&loop, 128) != 0) {
         printf("xev_loop_init failure\n");
         return 1;
     }
 
+    // Initialize a completion and a watcher. A completion is the actual
+    // thing a loop does for us, and a watcher is a high-level structure
+    // to help make it easier to use completions.
     xev_completion c;
     xev_watcher w;
+
+    // In this case, we initialize a timer watcher.
     if (xev_timer_init(&w) != 0) {
         printf("xev_timer_init failure\n");
         return 1;
     }
 
+    // Configure the timer to run in 1ms. This requires the completion that
+    // we actually configure to become a timer, and the loop that the
+    // completion should be registered with.
     xev_timer_run(&w, &loop, &c, 1, NULL, &timerCallback);
+
+    // Run the loop until there are no more completions.
     xev_loop_run(&loop, XEV_RUN_UNTIL_DONE);
 
     xev_timer_deinit(&w);
