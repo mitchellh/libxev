bca47650a6cab6a7f434da9c3f3c160fc595dd17
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2023-01-18 09:17:39 -0800

    website: setup folder to try it out

---
+++ website/pages/getting-started/concepts.mdx
@@ -0,0 +1,3 @@
+# General Concepts
+
+TODO
--- website/theme.config.jsx
+++ website/theme.config.jsx
@@ -1,16 +1,33 @@
+import { useRouter } from 'next/router'
+
 export default {
   logo: <span>libxev</span>,
+  docsRepositoryBase: 'https://github.com/mitchellh/libxev/blob/website/pages',
+
   project: {
     link: 'https://github.com/mitchellh/libxev',
   },
-  docsRepositoryBase: 'https://github.com/mitchellh/libxev/blob/website/pages',
+
   feedback: {
     content: null,
   },
+
   footer: {
     text: <span>
-      © {new Date().getFullYear()} <a href="https://github.com/mitchellh/libxev" target="_blank">libxev</a>
+      © {new Date().getFullYear()}
+      <a href="https://github.com/mitchellh/libxev" target="_blank">libxev</a>
     </span>,
-  }
-  // ...
+  },
+
+  useNextSeoProps() {
+    const { route } = useRouter()
+    if (route !== '/') {
+      return {
+        titleTemplate: '%s – libxev'
+      }
+    }
+  },
+
+  // Hide the last updated date.
+  gitTimestamp: <span></span>,
 }
