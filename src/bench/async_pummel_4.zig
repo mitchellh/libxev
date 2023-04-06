25834c215566808060c14f4822d632a4b1ea5a41
Corendos <corentin.godeau37@gmail.com>
2023-04-06 23:29:12 +0200

    fix: log level set incorrectly in benchmarks

--- src/bench/async_pummel_4.zig
+++ src/bench/async_pummel_4.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const run = @import("async_pummel_1.zig").run;
 
-pub const log_level: std.log.Level = .info;
+pub const std_options = struct {
+    pub const log_level: std.log.Level = .info;
+};
 
 pub fn main() !void {
     try run(4);
