1c4dd83d5b806a5d9a9fea03e62f79c3851b10d7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2024-06-08 19:21:40 -0700

    update flake to 0.13

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       # Other overlays
       (final: prev: rec {
         zigpkgs = inputs.zig.packages.${prev.system};
-        zig = zigpkgs.master;
+        zig = inputs.zig.packages.${prev.system}."0.13.0";
 
         # Latest versions
         wasmtime = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.wasmtime;
@@ -46,7 +46,7 @@
           nativeBuildInputs = with pkgs; [
             mandoc
             scdoc
-            zigpkgs.master
+            zig
 
             # Wasm
             wabt
